5d3725032267070d219cdcb7f656f6deffcf70d9
mathesbh <mathesbh@gmail.com>
2021-05-19 20:55:12 -0300

    Modifica tabela index

--- src/js/script.js
+++ src/js/script.js
@@ -17,19 +17,29 @@ const showQuestionnaries = (data) => {
   
   data.forEach(element => {
     const tbody = document.querySelector('#tbody');
-    const btn = document.createElement('button');
-    btn.innerText = 'Responder';
+    const btn = document.createElement('i');
+    const btn1 = document.createElement('i');
+    btn.classList.add('fas', 'fa-reply');
     btn.id = 'btn-answer';
     btn.setAttribute('onclick', `answerQuiz(${element.id})`);
+    btn1.classList.add('fas', 'fa-search-plus')
+    btn1.id = 'btn-show';
+    btn1.setAttribute('onclick', `showQuiz(${element.id})`);
     let title = document.createTextNode(element.title);
     let tr = tbody.insertRow();
     let td1 = tr.insertCell(0);
     td1.appendChild(title);
     let td2 = tr.insertCell(1);
     td2.appendChild(btn);
+    let td3 = tr.insertCell(2);
+    td3.appendChild(btn1);
   });
 }
 
 function answerQuiz(id){
   window.location.href = `../src/screens/answers.html?id=${id}`
 } 
+
+function showQuiz(id){
+  window.location.href = `../src/screens/showQuiz.html?id=${id}`
+}
